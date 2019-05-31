998088891fa811d15d735a36cb807056bfa304d3
LEE <wyliboyu@163.com>
2019-05-31 15:19:09 +0800

    修改department文件。

--- spirngcloud-api-demo/src/main/java/com/api/bean/Department.java
+++ spirngcloud-api-demo/src/main/java/com/api/bean/Department.java
@@ -13,7 +13,4 @@ public class Department implements Serializable {
     private Long dId; // 主键
     private String dName; // 部门名称
     private String dbSource;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
-
-
-
 }
